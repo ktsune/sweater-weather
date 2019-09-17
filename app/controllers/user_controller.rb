a82d26948aa95c28887244aa1a56995e44757394
Sarah Tatro <statrox4@gmail.com>
2019-09-17 15:00:21 -0600

    Create users upon registration

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,11 +2,15 @@ class UserController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
+    if @user.save!
+      render status: :created, json: { api_key: @user.api_key }
+    else
+      render html: "could not create a user"
+    end
   end
 
   private
+
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
