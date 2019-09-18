1d2706d722d1bd372278781139c3765f096a16f1
Sarah Tatro <statrox4@gmail.com>
2019-09-17 20:52:48 -0600

    Fix arbitrary method name

--- app/models/unsplash.rb
+++ app/models/unsplash.rb
@@ -3,11 +3,12 @@ class Unsplash
   attr_accessor :url, :id
 
   def initialize(city, state)
-    @response = response(city, state)
-    @url = @response[:urls][:regular]
+    response = get_image(city, state)
+    @url = response[:urls][:regular]
+    @id = response[:id]
   end
 
-  def response(city, state)
-    UnsplashService.fetch_gif(city, state)
+  def get_image(city, state)
+    UnsplashService.fetch_image(city, state)
   end
 end
