6a992c5fc917fc60a42ecc0d8ef5b33586df1e7c
Sarah Tatro <statrox4@gmail.com>
2019-09-17 19:23:10 -0600

    Create user or render status code & error message

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,11 @@
 class UserController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-    if @user.save!
-      render status: :created, json: { api_key: @user.api_key }
+    user = User.new(user_params)
+    if user.save!
+      render status: :created, json: { api_key: user.api_key }
     else
-      render html: "could not create a user"
+      render status: :bad_request, json: { error: user.errors.full_messages.to_sentence }
     end
   end
 
