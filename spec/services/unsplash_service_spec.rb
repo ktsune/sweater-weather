366bbe8ab356a2c9c4430c3d5e011baafcdf7984
Sarah Tatro <statrox4@gmail.com>
2019-09-17 23:13:37 -0600

    Background Refactors

--- spec/services/unsplash_service_spec.rb
+++ spec/services/unsplash_service_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 describe UnsplashService do
   scenario 'get city and state' do
-    response = UnsplashService.fetch_image('denver', 'CO')
+    response = UnsplashService.new.fetch_image('denver', 'CO')
     # binding.pry
 
-    expect(response).to be_an(Unsplash)
     expect(response).to be_a(Hash)
     # expect(response).to eq()
   end
