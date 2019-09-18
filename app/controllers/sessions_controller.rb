bca35b653914e29c153bfefea9003a8e92a86bcf
Sarah Tatro <statrox4@gmail.com>
2019-09-17 19:23:25 -0600

    Log in session or throw error code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
   def create
+    return not_authorized_error if !params[:email]
+    binding.pry
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      render status: :ok, json: { api_key: @user.api_key }
+      render status: :OK, json: { api_key: user.api_key }
     else
-      render html: 'Wrong email and/or password'
+      not_authorized_error
     end
+  end
+
+private
+
+  def not_authorized_error
+    render status: :unauthorized, json: { error: 'Re-enter your username and password' }
   end
 end
